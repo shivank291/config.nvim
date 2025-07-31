f860e3d949ce816516d9afde380bab98c7ec573b
shivankwt <shivang.arya19@gmail.com>
2025-07-31 10:58:11 +0530

    tried oil.nvim, it's shit

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,2 +1,7 @@
+vim.o.timeoutlen = 300
 vim.g.mapleader = " "
+
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>cs", vim.cmd.colorscheme)
+vim.keymap.set("i", "df", "<Esc>", { noremap = true, desc = "exit insert <df>" })
